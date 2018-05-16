41e682d36af91ad366c135837232e21589a46ff6
Juantheduckk <38923342+Juantheduckk@users.noreply.github.com>
2018-05-16 11:48:56 -0700

    Delete particle.js

--- particle.js
+++
@@ -1,6 +0,0 @@
-function Particle(loc){
-  this.loc = loc.copy();
-  this.acc = createVector(random(-.005, .005), random( -.005, .005));
-  this.vel = createVector(random(-1 , 1), random(-1 , 1));
-  this.lifespan = 255.0;
-  this.clr = clr;
