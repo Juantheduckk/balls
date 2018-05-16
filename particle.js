70b1a92a42d8555217bc2ea93f896e2dcf412eee
Juantheduckk <38923342+Juantheduckk@users.noreply.github.com>
2018-05-16 11:29:57 -0700

    Add files via upload

--- particle.js
+++ particle.js
@@ -1,6 +1,37 @@
-function Particle(loc){
-  this.loc = loc.copy();
+function Particle(loc, clr){
+  this.loc = createVector(width/2, height/2);
   this.acc = createVector(random(-.005, .005), random( -.005, .005));
-  this.vel = createVector(random(-1 , 1), random(-1 , 1));
-  this.lifespan = 255.0;
+  this.vel = createVector(random(-5.1 , 5.1), random(-5.1 , 5.1));
+  this.angle = random(PI/2);
   this.clr = clr;
+
+  this.lifespan = 200.0;
+
+   this.update = function(){
+    this.angle += 0.1;
+    this.vel.add(this.vel);
+    this.vel.limit(5.6);
+    this.loc.add(this.vel);
+    this.lifespan -= 3;
+    this.render();
+  };
+this.render = function(){
+
+    stroke(random(100, 200),random(100, 255), 255);
+    strokeWeight(5);
+    noFill();
+    var distToCenter = this.loc.dist(createVector(width/2, height/2))
+    push();
+    translate(this.loc.x, this.loc.y)
+    rotate(this.angle);
+    arc(0, 0, distToCenter, distToCenter, 0, PI/4);
+    pop();
+  };
+ this.isDead = function(){
+    if(this.lifespan < 0.0 ){
+      return true;
+    }else{
+      return false;
+    };
+  };
+}
--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
-var particleSystems = [];
+var pSystems = [];
 
 function setup() {
-    particleSystems.push(new Particalsystem (createVector(width/2, height/2)));
+    pSystems.push(new ParticleSystem (createVector(width/2, height/2)));
   var cnv = createCanvas(1100, 800);
   cnv.position((windowWidth - width)/2 , 30);
   frameRate(20);
@@ -10,20 +10,19 @@ function setup() {
 }
 
 function draw(){
-  //console.log("ps.length = " + particleSystems.length);
+
   fill(127, 50 , 90);
-  background(21);
-  for( var i = 0; i < particleSystems.length; i++){
-    particleSystems[i].addParticle();
-    particleSystems[i].run();
-  }
+  background(21, 2);
+  for( var i = 0; i < pSystems.length; i++){
+    pSystems[i].addParticle();
+    pSystems[i].run();
 
-  //text('Num Particles = '+ particle.length, 10, 30);
 
-  function mouseClicked(){
-    var moouseloc = createVector(mouseX, mouseY);
-    particleSystems.push(new particleSystem(mouseloc));
   }
 
 
+}
+function mouseClicked(){
+//  var mouseloc = createVector(mouseX, mouseY);
+pSystems.push(new ParticleSystem (createVector(mouseX, mouseY)));
 }
